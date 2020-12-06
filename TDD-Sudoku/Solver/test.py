a9812a8284b00d9585a72f5c8f781a7924ca4c3c
Máté Szabó <szmate1618@gmail.com>
2020-12-06 01:24:34 +0100

    make test stricter

--- TDD-Sudoku/Solver/test.py
+++ TDD-Sudoku/Solver/test.py
@@ -7,8 +7,13 @@ import solver
 class Test_test(unittest.TestCase):
     def test_solve_output_type(self):
         solution = solver.solve(test_data.solved)
+
         self.assertEquals(len(solution), 9)
         self.assertIsInstance(solution, list)
+
+        for line in solution:
+            self.assertEquals(len(line), 9)
+            self.assertIsInstance(line, list)
 
 if __name__ == '__main__':
     unittest.main()
