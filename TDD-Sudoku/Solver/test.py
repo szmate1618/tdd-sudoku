2f6323013fd9a0db6eddb0b8963cbe602febcfe5
Máté Szabó <szmate1618@gmail.com>
2020-12-06 01:45:53 +0100

    nitpicking

--- TDD-Sudoku/Solver/test.py
+++ TDD-Sudoku/Solver/test.py
@@ -7,16 +7,13 @@ import solver
 
 class TestTypes(unittest.TestCase):
 
-    def test_solve_output_type(self):
+    def test_output_type(self):
         solution = solver.solve(test_data.solved)
-
-        self.assertEquals(len(solution), 9)
+        self.assertEqual(len(solution), 9)
         self.assertIsInstance(solution, list)
-
         for line in solution:
-            self.assertEquals(len(line), 9)
+            self.assertEqual(len(line), 9)
             self.assertIsInstance(line, list)
-
             for cell in line:
                 self.assertIsInstance(cell, int)
 
